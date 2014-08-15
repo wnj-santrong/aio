5f288b81183a649ad6f4b6f49a97d88157ccbd53
weinianjie <weinianjie@weinianjie-pc>
2014-08-15 18:14:21 +0800

    把shell脚本目录转移到webapp下

--- shell/readme.txt
+++
@@ -1,4 +0,0 @@
-注意：正式发布的时候，需要将该目录下的所有shell文件放置到DirDefine.ShellDir所定义的目录下，并全部添加执行权限
-
-shell脚本说明如下：
-dbBackup.sh-----数据库备份
--- src/com/santrong/system/DirDefine.java
+++ src/com/santrong/system/DirDefine.java
@@ -8,7 +8,7 @@ package com.santrong.system;
 public class DirDefine {
 	// 部署环境
 	public static final String DbBackupDir 		= "/opt/AIO/Service/webservice/dbbackup";					// 数据库备份路径
-	public static final String ShellDir 		= "/opt/AIO/Service/webservice/shell";					// 所有shell脚本目录
+	public static final String ShellDir 		= "/opt/AIO/Service/webservice/webapp/shell";				// 所有shell脚本目录
 	public static final String SysConfigDir 	= "/opt/AIO/Service";									// 公共配置文件目录
 	public static final String VedioDir 		= "/RecData";										// 录制的视频目录
 ////public static final String VedioDir 		= "/data";										// 录制的视频目录
